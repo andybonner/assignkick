8dfcdd1ce9b11ef653baa9fad1bc0b711476e090
Daniel Kim <dskay7@gmail.com>
2017-10-21 10:24:09 -0400

    minor style update

--- client/src/components/Solution/Solution.js
+++ client/src/components/Solution/Solution.js
@@ -5,7 +5,7 @@ import "./Solution.css";
 // Solution component
 const Solution = props =>
   <section className="solution-container">
-    <h3 className="solution-title">{ props.header }</h3>
+    <h3 className="solution-title text-center">{ props.header }</h3>
 
     <div className="row">
       <div className="col-sm-6">
