0f388ded480004a2d27419e9297feb89d1800429
Daniel Kim <dskay7@gmail.com>
2017-10-22 02:34:52 -0400

    latest update

--- client/src/components/Forms/AssignForm/AssignForm.js
+++ client/src/components/Forms/AssignForm/AssignForm.js
@@ -43,6 +43,18 @@ class AssignmentForm extends Component {
     callback();
   }
 
+  loadAssignments = () => {
+    axios.get('/api/assignments/')
+    .then(result=> {
+      console.log(result.data);
+
+      this.setState({
+        assignments: result.data
+      });
+
+    })
+  }
+
   handleReset = () => this.props.form.resetFields();
 
   render() {
@@ -159,7 +171,7 @@ class AssignmentForm extends Component {
           {...tailFormItemLayout}
           className={ this.props.regClass }
         >
-          <Button type="primary" htmlType="submit" style={{ marginBottom: 20 }} onClick={this.props.loadAssignments} >Add Assignment</Button>
+          <Button type="primary" htmlType="submit" style={{ marginBottom: 20 }} >Add Assignment</Button>
           
           <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
               Clear
