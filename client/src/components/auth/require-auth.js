cf49511ee49d6df223a3ab4f9d71a2d581996039
Andy Bonner <abonner@duke.edu>
2017-10-29 10:41:14 -0400

    fix history push method

--- client/src/components/auth/require-auth.js
+++ client/src/components/auth/require-auth.js
@@ -9,13 +9,13 @@ export default function(ComposedComponent) {
 
     componentWillMount() {
       if(!this.props.authenticated) {
-        this.context.router.push('/');
+        this.context.router.history.push('/');
       }
     }
 
     componentWillUpdate(nextProps) {
       if(!nextProps.authenticated) {
-        this.context.router.push('/');
+        this.context.router.history.push('/');
       }
     }
 
